0df5c59a8ecbff2a5d3c792d4a280c8dd4666f94
Brandon C. Lewis <raletheblade@hotmail.com>
2022-03-16 10:05:01 -0500

    ShouldThrowDependencyExceptionOnRegisterIfInternalServerErrorOccursAndLogItAsync -> FAIL

--- OtripleS.Portal.Web.Tests.Unit/Services/Students/StudentServiceTests.Exception.cs
+++ OtripleS.Portal.Web.Tests.Unit/Services/Students/StudentServiceTests.Exception.cs
@@ -124,5 +124,45 @@ namespace OtripleS.Portal.Web.Tests.Unit.Services.Students
             this.apiBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnRegisterIfInternalServerErrorOccursAndLogItAsync()
+        {
+            // given
+            Student someStudent = CreateRandomStudent();
+            string exceptionMessage = GetRandomString();
+            var responseMessage = new HttpResponseMessage();
+
+            var httpResponseInternalServerErrorException =
+                new HttpResponseInternalServerErrorException(
+                    responseMessage: responseMessage,
+                    message: exceptionMessage);
+
+            var expectedStudentDependencyException =
+                new StudentDependencyException(httpResponseInternalServerErrorException);
+
+            this.apiBrokerMock.Setup(broker =>
+                broker.PostStudentAsync(someStudent))
+                    .ThrowsAsync(httpResponseInternalServerErrorException);
+
+            // when
+            ValueTask<Student> registerStudentTask =
+                this.studentService.RegisterStudentAsync(someStudent);
+
+            // then
+            await Assert.ThrowsAsync<StudentDependencyException>(() =>
+                registerStudentTask.AsTask());
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.PostStudentAsync(It.IsAny<Student>()),
+                Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedStudentDependencyException))),
+                Times.Once);
+
+            this.apiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
--- OtripleS.Portal.Web/Services/Students/StudentService.Exceptions.cs
+++ OtripleS.Portal.Web/Services/Students/StudentService.Exceptions.cs
@@ -30,6 +30,10 @@ namespace OtripleS.Portal.Web.Services.Students
             {
                 throw CreateAndLogCriticalDependencyException(httpResponseUnauthorizedException);
             }
+            catch (HttpResponseConflictException httpResponseConflictException)
+            {
+                throw CreateAndLogDependencyValidationException(httpResponseConflictException);
+            }
             catch (HttpResponseBadRequestException httpResponseBadRequestException)
             {
                 throw CreateAndLogDependencyValidationException(httpResponseBadRequestException);
