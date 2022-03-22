781f74dea6f35761ae6eb09549c22934a68012b8
Brandon C. Lewis <raletheblade@hotmail.com>
2022-03-22 15:19:05 -0500

    ShouldThrowDependencyValidationExceptionOnAddIfStudentValidationErrorOccurredAndLogItAsync - FAIL

---
+++ OtripleS.Portal.Web/Models/StudentViews/Exceptions/StudentViewDependencyValidationException.cs
@@ -0,0 +1,11 @@
+﻿namespace OtripleS.Portal.Web.Models.StudentViews.Exceptions
+{
+    public class StudentViewDependencyValidationException : Exception
+    {
+        public StudentViewDependencyValidationException(Exception innerException)
+            : base("Student view dependency error occurred, try again.", innerException)
+        {
+
+        }
+    }
+}
